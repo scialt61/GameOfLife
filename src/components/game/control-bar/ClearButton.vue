558a350def98dfc1f949cdf41af854d190e25b8f
milesman34 <miles.saunders@icloud.com>
2020-07-24 17:03:23 -0400

    Preparing for clear button merge

---
+++ src/components/game/control-bar/ClearButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="control-button" @click="clear">Clear</div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            // Clears the grid and pauses the game
+            clear() {
+                this.clearCells();
+
+                // Resets the game
+                this.setActive(false);
+                this.setTimesPaused(0);
+            }
+        }
+    };
+</script>
--- src/components/game/control-bar/ControlBar.vue
+++ src/components/game/control-bar/ControlBar.vue
@@ -1,14 +1,17 @@
 <template>
     <div id="control-bar">
         <play-button />
+        <clear-button />
     </div>
 </template>
 
 <script>
+    import ClearButton from "./ClearButton.vue";
     import PlayButton from "./PlayButton.vue";
 
     export default {
         components: {
+            "clear-button": ClearButton,
             "play-button": PlayButton
         }
     };
@@ -18,8 +21,29 @@
     #control-bar {
         display: grid;
 
-        grid-template-columns: 25% auto;
+        grid-template-columns: 25% 25% auto;
 
         border-top: solid 3px rgb(25, 25, 25);
+    }
+</style>
+
+<style>
+    .control-button {
+        display: flex;
+
+        justify-content: center;
+        align-items: center;
+
+        color: rgb(255, 255, 255);
+
+        font-size: 200%;
+
+        font-family: "Helvetica";
+
+        user-select: none;
+    }
+
+    .control-button:hover {
+        background-color: rgb(45, 45, 45);
     }
 </style>
